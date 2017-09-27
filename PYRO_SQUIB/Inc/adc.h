336ea9ff2145898ebf14e4d294c877c023afa2c4
nowhard <nowhard@mail.ru>
2017-09-27 10:45:49 +0300

    Калибровка каналов ацп пиропатронов

--- PYRO_SQUIB/Inc/adc.h
+++ PYRO_SQUIB/Inc/adc.h
@@ -6,10 +6,10 @@
 #define ADC_CHN_NUM					8//всего каналов ацп
 #define ADC_CHN_POT_NUM			4//каналы ацп на регулируемых выходах пиропатронов
 
-#define ADC_CALIBR_MIN_VAL	0
-#define ADC_CALIBR_MIN_CUR	0.0 //Current - Amp.
+#define ADC_CALIBR_MIN_VAL	0x000
+#define ADC_CALIBR_MIN_CUR	0.03 //Current - Amp.
 
-#define ADC_CALIBR_MAX_VAL	0x7E0
+#define ADC_CALIBR_MAX_VAL	0x700//0x7E0
 #define ADC_CALIBR_MAX_CUR	1.0 //Current - Amp.
 
 void ADC_Init(void);
